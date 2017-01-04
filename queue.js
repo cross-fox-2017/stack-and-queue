e80bd8c639801cd64c3bcc3c74e289aab362ab2e
Mangku Widodo & Samuel Stauffer <pair+mangkuwi.samuel@devbootcamp.com>
2017-01-04 21:05:23 +0700

    finish

--- queue.js
+++ queue.js
@@ -1,5 +1,25 @@
 "use strict"
 
 class Queue {
-  constructor() {}
+  constructor() {
+    this.max = 5
+    this.arr = []
+  }
+  push(x) {
+    this.arr.push(x)
+    return this
+  }
+
+  peek() {
+    this.arr.shift()
+    return this
+  }
+
 }
+let bulok = new Queue()
+console.log(bulok.push(1))
+console.log(bulok.push(2))
+console.log(bulok.push(3))
+console.log(bulok.push(4))
+// console.log(this.arr);
+console.log(bulok.peek());
--- stack.js
+++ stack.js
@@ -2,18 +2,57 @@
 
 class Stack {
   constructor() {
+    this.max = 10
+    this.arr = []
   }
 
   push(x) {
+    // console.log(this.arr);
+    // console.log(this.max);
+    if (this.arr.length < this.max) {
+      this.arr.push(x)
+      return `Add data: ${x}`
+    }else {
+      return this.isFull()
+    }
   }
 
   pop() {
+    if (this.arr.length > 1) {
+      this.arr.pop()
+      return `Data sudah dihapus`
+    }else {
+      return this.isEmpty()
+    }
   }
 
   peek() {
-
+    return this.arr.shift()
   }
 
   isEmpty() {
+    if (this.arr.length == 0) {
+      return 'Data kosong'
+    }
+  }
+
+  isFull(){
+    if (this.arr.length >= this.max) {
+      return `Data Penuh`
+    }
   }
 }
+
+let bulok = new Stack()
+
+// console.log(bulok.push(1))
+// console.log(bulok.push(2))
+// console.log(bulok.push(4))
+// console.log(bulok.push(5))
+// console.log(bulok.push(6))
+// console.log(bulok.push(7))
+// console.log(bulok.push(8))
+// console.log(bulok.push(9))
+// console.log(bulok.push(10))
+// console.log(bulok.peek());
+console.log(bulok.pop());
