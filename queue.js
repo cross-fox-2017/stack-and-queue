52ba041a114ce13da8a4f4ee21271f01f2985479
Irsan Sebastian <email": null>
2017-01-05 10:39:07 +0700

    First Commit

--- queue.js
+++ queue.js
@@ -1,5 +1,46 @@
 "use strict"
 
 class Queue {
-  constructor() {}
+
+  constructor() {
+    this.arr = [];
+  }
+
+  push(x){
+    return this.arr.push(x);
+  }
+
+  pop(){
+
+    return this.arr.shift();
+  }
+
+  peek(){
+    return this.arr;
+  }
+
+  isEmpty() {
+
+    if(this.arr.length === 0){
+
+      return "|IS EMPTY|";
+    }
+    else {
+      return this.peek();
+    }
+  }
 }
+
+let my_queue = new Queue();
+
+console.log(my_queue.isEmpty());
+
+my_queue.push("HACKTIV8");
+my_queue.push("HACKTIV9");
+my_queue.push("HACKTIV10");
+
+console.log(my_queue.isEmpty());
+
+my_queue.pop();
+
+console.log(my_queue.peek());
--- stack.js
+++ stack.js
@@ -1,19 +1,48 @@
 "use strict"
 
 class Stack {
+
   constructor() {
+
+    this.arr = [];
   }
 
-  push(x) {
+  push(x){
+    
+    return this.arr.push(x);
   }
 
-  pop() {
+  pop(){
+
+    return this.arr.pop();
   }
 
-  peek() {
-
+  peek(){
+    return this.arr;
   }
 
   isEmpty() {
+
+    if(this.arr.length === 0){
+
+      return "|IS EMPTY|";
+    }
+    else {
+      return this.peek();
+    }
   }
 }
+
+let my_stack = new Stack();
+
+console.log(my_stack.isEmpty());
+
+my_stack.push("HACKTIV8");
+my_stack.push("HACKTIV9");
+my_stack.push("HACKTIV10");
+
+console.log(my_stack.isEmpty());
+
+my_stack.pop();
+
+console.log(my_stack.peek());
