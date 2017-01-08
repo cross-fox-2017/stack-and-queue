c79255e1297d950aafcb48348fae58c2219c7c6d
Raditya <arya.raditya26@gmail.com>
2017-01-08 23:02:19 +0700

    Done all releases

--- queue.js
+++ queue.js
@@ -1,5 +1,57 @@
 "use strict"
 
 class Queue {
-  constructor() {}
+  constructor() {
+    this.inputArr = [];
+  }
+
+  push(input) {
+    this.inputArr.push(input)
+  }
+
+  shift() {
+    this.inputArr.shift()
+    return this
+  }
+
+  peek() {
+    return this.inputArr
+  }
+
+  isEmpty() {
+    if (this.inputArr.length == 0) {
+      return 'Yes! The queue is empty'
+    }
+    else {
+      return `${this.inputArr.length} item(s) in queue`
+    }
+  }
 }
+
+let createQueue = new Queue()
+
+console.log(`The queue is empty? ${createQueue.isEmpty()}`);
+
+// push 3 items into queue
+createQueue.push('JavaScript')
+createQueue.push('is just so')
+createQueue.push('cool')
+
+// test
+console.log(`The queue is empty? ${createQueue.isEmpty()}`);
+console.log(createQueue.peek());
+
+// 1st shift
+createQueue.shift();
+console.log(`The Queue is empty? ${createQueue.isEmpty()}`);
+console.log(createQueue.peek());
+
+// 2nd shift
+createQueue.shift();
+console.log(`The queue is empty? ${createQueue.isEmpty()}`);
+console.log(createQueue.peek());
+
+// 3nd shift
+createQueue.shift();
+console.log(`The queue is empty? ${createQueue.isEmpty()}`);
+console.log(createQueue.peek());
--- stack.js
+++ stack.js
@@ -2,18 +2,56 @@
 
 class Stack {
   constructor() {
+    this.inputArr = [];
   }
 
-  push(x) {
+  push(input) {
+    this.inputArr.push(input)
   }
 
   pop() {
+    this.inputArr.pop()
+    return this
   }
 
   peek() {
-
+    return this.inputArr
   }
 
   isEmpty() {
+    if (this.inputArr.length == 0) {
+      return 'Yes! The stack is empty'
+    }
+    else {
+      return `${this.inputArr.length} item(s) in stack`
+    }
   }
 }
+
+let createStack = new Stack()
+
+console.log(`The stack is empty? ${createStack.isEmpty()}`);
+
+// push 3 items into stack
+createStack.push('JavaScript')
+createStack.push('is just so')
+createStack.push('cool')
+
+// test
+console.log(`The stack is empty? ${createStack.isEmpty()}`);
+console.log(createStack.peek());
+
+// 1st pop
+createStack.pop();
+console.log(`The stack is empty? ${createStack.isEmpty()}`);
+console.log(createStack.peek());
+
+// 2nd pop
+createStack.pop();
+console.log(`The stack is empty? ${createStack.isEmpty()}`);
+console.log(createStack.peek());
+
+// 3nd pop
+createStack.pop();
+console.log(`The stack is empty? ${createStack.isEmpty()}`);
+console.log(createStack.peek());
