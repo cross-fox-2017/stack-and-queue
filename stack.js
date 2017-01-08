dbb81b7df43ebc79fbf400510f3ce5e490efd50e
unknown <yaumashf@gmail.com>
2017-01-08 22:16:45 +0700

    release0 done

--- stack.js
+++ stack.js
@@ -5,9 +5,8 @@ class Stack {
     this.arr = [];
   }
   push(x) {
-    if (this.arr.length===5) {
-      return console.log('Can not push, data is full');
-    }
+    if (this.arr.length===10)
+      return this.isFull();
     return this.arr.push(x);
   }
   pop() {
@@ -17,31 +16,38 @@ class Stack {
     return this.arr.shift();
   }
   isEmpty() {
-    if(this.arr.length === 0 ){
+    if(this.arr.length === 0 )
       return `Y`;
-    }
     return`N`;
+  }
+  isFull() {
+    return console.log('Can not push, data is full');
+  }
+  print() {
+    return this.arr;
   }
 }
 
 let my_stack = new Stack();
 
 console.log(`The stack is empty? ${my_stack.isEmpty()}`);
-my_stack.push("JAVASCRIPT");
+my_stack.push("JAVASCRIPT"); //peek
 my_stack.push("is just so");
 my_stack.push("cool");
 my_stack.push("Hacktiv8");
 my_stack.push("Learn");
 my_stack.push("code");
 my_stack.push("PHP");
-my_stack.push("MySql");
+my_stack.push("MySql"); //pop
+my_stack.push("C++"); //pop
+my_stack.push("Ruby"); //pop
 
-my_stack.push(15);
+my_stack.push(15); //can't push
 
 console.log(my_stack.pop());
 console.log(my_stack.pop());
 console.log(my_stack.pop());
 
 console.log(my_stack.peek());
-
+console.log(my_stack.print());
 console.log(`The stack is empty? ${my_stack.isEmpty()}`);
