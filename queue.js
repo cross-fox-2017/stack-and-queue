89701cd9ccd9ba2b84a9ada8b9773d5cc96890ed
qblol <iqbal.0809@gmail.com>
2017-01-09 08:50:37 +0700

    done

--- queue.js
+++ queue.js
@@ -1,5 +1,50 @@
 "use strict"
 
 class Queue {
-  constructor() {}
+  constructor() {
+    this.array = []
+  }
+
+  push(x) {
+    return this.array.push(x);
+  }
+
+  shift() {
+    return this.array.shift();
+  }
+
+  peek() {
+    return this.array;
+  }
+
+  isEmpty() {
+    if (this.array.length == 0) {
+      return "yes"
+    } else if (this.array.length > 0) {
+      return "no";
+    }
+
+  }
 }
+
+let my_stack = new Queue()
+
+console.log(`The stack is empty? ${my_stack.isEmpty()}`)
+
+my_stack.push("JavaScript")
+my_stack.push("is just so")
+my_stack.push("cool")
+
+my_stack.push(15)
+my_stack.push(14)
+
+console.log(my_stack.shift())
+console.log(my_stack.shift())
+console.log(my_stack.shift())
+console.log(my_stack.shift())
+
+console.log(`The stack is empty? ${my_stack.isEmpty()}`)
+
+console.log(my_stack.shift())
+
+console.log(`The stack is empty? ${my_stack.isEmpty()}`)
--- stack.js
+++ stack.js
@@ -2,18 +2,49 @@
 
 class Stack {
   constructor() {
+    this.array = []
   }
 
   push(x) {
+    return this.array.push(x);
   }
 
   pop() {
+    return this.array.pop();
   }
 
   peek() {
-
+    return this.array;
   }
 
   isEmpty() {
+    if (this.array.length == 0) {
+      return "yes"
+    } else if (this.array.length > 0) {
+      return "no";
+    }
+
   }
 }
+
+let my_stack = new Stack()
+
+console.log(`The stack is empty? ${my_stack.isEmpty()}`)
+
+my_stack.push("JavaScript")
+my_stack.push("is just so")
+my_stack.push("cool")
+
+my_stack.push(15)
+my_stack.push(14)
+
+console.log(my_stack.pop())
+console.log(my_stack.pop())
+console.log(my_stack.pop())
+console.log(my_stack.pop())
+
+console.log(`The stack is empty? ${my_stack.isEmpty()}`)
+
+console.log(my_stack.pop())
+
+console.log(`The stack is empty? ${my_stack.isEmpty()}`)
