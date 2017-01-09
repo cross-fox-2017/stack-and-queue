bde05995210bc2e0ddcd52983b202425c9e841a7
kahmadsyt <achmadkamil17@gmail.com>
2017-01-09 21:49:34 +0700

    Tugas Stack and Queue

--- stack.js
+++ stack.js
@@ -2,18 +2,51 @@
 
 class Stack {
   constructor() {
+    this.array = [];
   }
 
-  push(x) {
+  push(val) {
+    return this.array.push(val);
   }
 
   pop() {
+    return this.array.pop();
   }
 
   peek() {
+    return this.array
 
   }
 
   isEmpty() {
+    if(this.array.length == 0){
+      return 'Stack is Empty!'
+    } else {
+      return `${this.array.length}`
+    }
   }
 }
+
+let stackArr = new Stack();
+console.log(`The stack is empty? ${stackArr.isEmpty()}`);
+
+// Penambahan 3 Array berupa string
+stackArr.push('JavaScript')
+stackArr.push('is just so')
+stackArr.push('cool')
+
+// Pengecekkan Array sampai empty
+console.log(`The stack is empty? ${stackArr.isEmpty()}`);
+console.log(stackArr.peek());
+
+stackArr.pop();
+console.log(`The stack is empty? ${stackArr.isEmpty()}`);
+console.log(stackArr.peek());
+
+stackArr.pop();
+console.log(`The stack is empty? ${stackArr.isEmpty()}`);
+console.log(stackArr.peek());
+
+stackArr.pop();
+console.log(`The stack is empty? ${stackArr.isEmpty()}`);
+console.log(stackArr.peek());
