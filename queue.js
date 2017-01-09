9c6868a943ecaf96d9af10554553ccaf7f20599a
didietsuryadi <didietsuryadi@gmail.com>
2017-01-09 20:45:51 +0700

    stack and queue finish

--- queue.js
+++ queue.js
@@ -1,5 +1,42 @@
 "use strict"
 
 class Queue {
-  constructor() {}
+  constructor() {
+    this._array = [];
+  }
+  unshift(x) {
+    if(this._array.length > 10){
+      this.isFull();
+    }
+    return this._array.unshift(x);
+  }
+  shift() {
+    return this._array.shift();
+  }
+  peek() {
+    return this._array[0];
+  }
+  isEmpty() {
+    if (this._array.length === 0) {
+      return `Empty`;
+    }
+    else {
+      return `Available`;
+    }
+  }
+  isFull() {
+      return console.log("Array is Full");
+  }
 }
+let my_queue = new Queue();
+console.log(`The queue is empty? ${my_queue.isEmpty()}`);
+my_queue.unshift("HACKTIVEIGHT");
+my_queue.unshift("HACKTIV8");
+my_queue.unshift("HACKTIVATE");
+my_queue.unshift(15);
+
+console.log(my_queue.shift());
+console.log(my_queue.shift());
+console.log(my_queue.shift());
+console.log(`The queue is empty? ${my_queue.isEmpty()}`);
+console.log(my_queue.shift());
--- stack.js
+++ stack.js
@@ -2,18 +2,44 @@
 
 class Stack {
   constructor() {
+    this._array = [];
   }
 
   push(x) {
+    return this._array.push(x)
+
   }
 
   pop() {
+    return this._array.pop();
   }
 
   peek() {
-
+    return this._array.slice(-1)[0];
   }
 
   isEmpty() {
+    if(this._array.length == 0){
+      return "stack is empty"
+    }
+    else if(this._array.length == 15){
+      return "stack is full"
+    } else{
+      return "stack terisi"
+    }
   }
 }
+
+let my_stack = new Stack();
+
+console.log(my_stack.isEmpty());
+
+my_stack.push("HACKTIVATE");
+my_stack.push("HACKTIV8");
+my_stack.push("HACKTIVEIGHT");
+
+console.log(my_stack.isEmpty());
+
+my_stack.pop();
+
+console.log(my_stack.peek());
