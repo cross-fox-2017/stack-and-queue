0e1e9bf1b3a783386cc4cee0f1de37bc49ed1322
Wahyuhidayatt <wahyuhdyt95@gmail.com>
2017-01-11 07:43:39 +0700

    stack and queu

--- queue.js
+++ queue.js
@@ -1,5 +1,50 @@
 "use strict"
 
 class Queue {
-  constructor() {}
+  constructor() {
+    this.array = []
+  }
+
+  push(val) {
+    return this.array.push(val);
+  }
+
+  shift() {
+    return this.array.shift();
+  }
+
+  peek() {
+    return this.array;
+  }
+
+  isEmpty() {
+    if (this.array.length == 0) {
+      return "Kosong"
+    } else if (this.array.length > 0) {
+      return "Penuh";
+    }
+
+  }
 }
+
+let stackNew = new Queue()
+
+console.log(`The stack is empty? ${stackNew.isEmpty()}`)
+
+stackNew.push("Belajar")
+stackNew.push("JavaScript")
+stackNew.push("Seru")
+
+stackNew.push("Tapi")
+stackNew.push("pusing")
+
+console.log(stackNew.shift())
+console.log(stackNew)
+console.log(stackNew.shift())
+console.log(stackNew)
+console.log(stackNew.shift())
+console.log(stackNew)
+console.log(stackNew.shift())
+console.log(stackNew)
+console.log(stackNew.shift())
+console.log(`The stack ${stackNew.isEmpty()}`)
--- stack.js
+++ stack.js
@@ -1,19 +1,50 @@
 "use strict"
-
 class Stack {
   constructor() {
+    this.array = []
   }
 
-  push(x) {
+  push(val) {
+    return this.array.push(val);
   }
 
   pop() {
+    return this.array.pop();
   }
 
   peek() {
-
+    return this.array;
   }
 
   isEmpty() {
+    if (this.array.length == 0) {
+      return "kosong"
+    } else if (this.array.length > 0) {
+      return "penuh";
+    }
+
   }
 }
+
+let stackNew = new Stack()
+
+console.log(`The stack is empty? ${stackNew.isEmpty()}`)
+
+stackNew.push("Belajar")
+stackNew.push("JavaScript")
+stackNew.push("seru")
+
+stackNew.push("Tapi")
+stackNew.push("Pusing")
+
+console.log(stackNew.pop())
+console.log(stackNew)
+console.log(stackNew.pop())
+console.log(stackNew)
+console.log(stackNew.pop())
+console.log(stackNew)
+console.log(stackNew.pop())
+console.log(stackNew)
+console.log(stackNew.pop())
+console.log(`The stack ${stackNew.isEmpty()}`)
+
